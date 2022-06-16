f4b1c56170ff163a710c4a7b5cc557294c0c06ed
rdominguez <rdominguez@rdominguez.uaa.local>
2022-06-16 17:08:06 -0400

    init commit

---
+++ src/main/java/uaa/control/modelo/Pedido.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uaa.control.modelo;
+
+/**
+ *
+ * @author rdominguez
+ */
+public class Pedido {
+    int nroPedido;
+    String fecha;
+    String hora;
+    int codigoContable;
+    Producto producto;
+    int cantidad;
+    double precio;
+}
